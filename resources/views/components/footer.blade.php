b6114a58533b81085f896a6e24dd5f5d6538bb35
Jordy-lizard <d270021@edu.curio.nl>
2025-03-18 13:43:06 +0100

    ticekt 6 footer klaar

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,37 @@
 
 <footer>
-	© {{ __('misc.copyright') }}
-</footer>
+    <div class="footer-content">
+
+      <div class="footer-about">
+        <h3>Over ons</h3>
+        <p>Wij zijn een innovatief bedrijf dat zich richt op het leveren van de beste producten en diensten voor onze klanten. Onze missie is om de wereld te verbeteren door middel van technologie en duurzaamheid.</p>
+      </div>
+
+
+      <div class="footer-contact">
+        <h3>Contactgegevens</h3>
+        <ul>
+          <li><strong>Adres:</strong> Straatnaam 123, 1000 Stad</li>
+          <li><strong>Email:</strong> info@voorbeeld.nl</li>
+          <li><strong>Telefoon:</strong> +31 20 123 4567</li>
+        </ul>
+      </div>
+
+      <div class="footer-social">
+        <h3>Volg ons</h3>
+        <ul>
+          <li><a href="https://www.facebook.com" target="_blank">Facebook</a></li>
+          <li><a href="https://www.twitter.com" target="_blank">Twitter</a></li>
+          <li><a href="https://www.instagram.com" target="_blank">Instagram</a></li>
+          <li><a href="https://www.linkedin.com" target="_blank">LinkedIn</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="footer-bottom">
+      <p>© {{ __('misc.copyright') }}</p>
+    </div>
+  </footer>
 
 
 <!-- analytics code -->
